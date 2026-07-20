d09cf4d17cb1d1b17ed4ceb8c2cabe4cfafa2548
Abel Acosta <abel.2833@hotmail.com>
2026-07-19 23:13:49 -0500

    refactor(app): remove unused User model and update seeder

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Modules\Administration\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,10 +17,5 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
